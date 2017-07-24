687a91bdf89ba86b95cff3ea69d3ebf3e89cc3be
EthanReese <ethan.reese01@gmail.com>
2017-07-24 12:41:53 -0700

    Started making the read in class

--- src/SchedulingApp.java
+++ src/SchedulingApp.java
@@ -1,7 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  * Created by Ethan Reese, Aletea VanVeldhuesen, and Josh Bromley on 7/24/17.
  */
+
+
+
 public class SchedulingApp {
+
+    BufferedReader br = null;
     public SchedulingApp(){
 
 
@@ -12,4 +22,15 @@ public class SchedulingApp {
     public static void main(String[] args){
         new SchedulingApp();
     }
+
+    public void readForecasting(String filePath){
+        //Attempt to read in the file
+        try{
+            br = new BufferedReader(new FileReader(filePath));
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+            //Todo: Tell the user to input a new forecasting file
+        }
+    }
+
 }
