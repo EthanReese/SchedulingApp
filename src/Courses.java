6bf5576a4303f9626ac17742e42bb7df15741884
joshua-bromley <joshua@mattbromley.com>
2017-07-24 14:04:16 -0700

    Added Getters & Setters in Courses

--- src/Courses.java
+++ src/Courses.java
@@ -1,6 +1,45 @@
 import java.util.ArrayList;
 
 public class Courses {
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public Boolean getRequried() {
+        return requried;
+    }
+
+    public void setRequried(Boolean requried) {
+        this.requried = requried;
+    }
+
+    public Float getCreditAmount() {
+        return creditAmount;
+    }
+
+    public void setCreditAmount(Float creditAmount) {
+        this.creditAmount = creditAmount;
+    }
+
+    public ArrayList<String> getStudentsInCourse() {
+        return studentsInCourse;
+    }
+
+    public void setStudentsInCourse(ArrayList<String> studentsInCourse) {
+        this.studentsInCourse = studentsInCourse;
+    }
+
+    public ArrayList<String> getTeachersTeachingCourse() {
+        return teachersTeachingCourse;
+    }
+
+    public void setTeachersTeachingCourse(ArrayList<String> teachersTeachingCourse) {
+        this.teachersTeachingCourse = teachersTeachingCourse;
+    }
 
     String courseCode;
     Boolean requried;
