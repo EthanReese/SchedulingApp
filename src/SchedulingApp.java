35822bcaf83c17b3088ea913da2f452b698c8558
EthanReese <ethan.reese01@gmail.com>
2017-07-24 13:27:17 -0700

    Finished the class to read in the Array with the forecasting requests

--- src/SchedulingApp.java
+++ src/SchedulingApp.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Scanner;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Ethan Reese, Aletea VanVeldhuesen, and Josh Bromley on 7/24/17.
@@ -23,14 +26,36 @@ public class SchedulingApp {
         new SchedulingApp();
     }
 
-    public void readForecasting(String filePath){
+    public ArrayList<ArrayList<String>> readForecasting(String filePath){
+        //Make a proper arraylist to return
+        ArrayList<ArrayList<String>> returnList= new ArrayList<ArrayList<String>>();
+        int counter = 0;
         //Attempt to read in the file
         try{
+            //Make a buffered reader that can read in the csv file
             br = new BufferedReader(new FileReader(filePath));
+            while((br.readLine()) != null){
+                ArrayList<String> tempList = new ArrayList<String>(Arrays.asList((br.readLine()).split(",")));
+                returnList.add(counter, tempList);
+                counter += 1;
+            }
+
         }catch(FileNotFoundException e){
             e.printStackTrace();
             //Todo: Tell the user to input a new forecasting file
+        }catch(IOException e){
+            e.printStackTrace();
+            //idk how the user is supposed to fix that
+        }finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+        return returnList;
     }
 
 }
